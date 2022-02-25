af00c2e48a6c50de7f2ed081e9be04ce97bc186d
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-02-25 11:50:32 -0800

    Implemented expressJS and completed basic project requirements

---
+++ expressServer.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+
+app.get('/pets', function(req, res) {
+    fs.readFile('./pets.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        res.setHeader('Content-Type', 'application/json').status(200).send(jsonString);
+    })
+})
+
+app.get('/pets/:index', function(req, res) {
+    fs.readFile('./pets.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        const parsedJSON = JSON.parse(jsonString);
+        if (parsedJSON[req.params.index]) {
+            res.setHeader('Content-Type', 'application/json').status(200).json(parsedJSON[req.params.index]);
+        } else {
+            res.setHeader('Content-Type', 'text/plain').status(404).send('Not a valid index');
+        }
+
+    })
+})
+
+app.listen(8080, function() {
+    console.log('Server is running')
+})
